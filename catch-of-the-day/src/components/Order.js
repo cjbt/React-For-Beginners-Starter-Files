f4cd71d729ab1100facb6894d375e850dbc54f05
cecil john tantay <cjbt@iMac.local>
2018-10-11 10:46:15 -0700

    initial commit, some bugs needs to be fixed

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.component {
+  render() {
+    return (
+      <div className="order">
+        Order!!
+      </div>
+    )
+  }
+}
+
+export default Order;
